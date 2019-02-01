02e9c9001ca74f1078989145f1b9d5d2db1d04ad
김혜진 <asdzxc514@naver.com>
2019-02-01 16:35:33 +0900

    CRUD 성공, readme 수정

--- README.md
+++ README.md
@@ -4,4 +4,69 @@
 `pm2 -v`  //3.2.4 확인<br>
 `pm2 start main.js`<br>
 `pm2 list` <br>
-- watching 이 disabled 면 pm2 stop main 후 pm2 start main.js --watch 해주기!<br>
+- watching 이 disabled 면 pm2 stop main 후 pm2 start main.js --watch 해주기!<br><br>
+
+
+
+
+* **삭제 전 물어보기**<br>
+```
+<form action="delete_process" method="post" onsubmit="return confirm('진짜 삭제할거야?')"><br><br>
+```
+
+
+- #### **pm2를 실행할 때**
+ --watch 옵션을 주면 파일이 변경되었을 때 앱을 리로드하게 됩니다. 즉 data 디렉토리의 파일이 수정되었을 때 리로드가 일어나게 되는 것이죠. 이런 문제를 방지하기 위해서는 data 디렉토리에 대해서는 watch를 하지 않도록 설정해야 합니다. 아래의 방법이 도움이 될 것입니다. 
+```
+pm2 delete main
+pm2 start main.js --watch --ignore-watch="data/*"
+```
+<br><br>
+
+```
+var http = require('http');
+var server = http.createServer(funcion(request, response){
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    response.end('Hello node.js!');
+    });
+```
+
+`require()`  <br>
+1. dynamic loading을 할 수 있다.<br>
+2. synchronous (여러 개를 require하면 순차적으로 처리)<br><br>
+
+
+`import` (from ES6)<br>
+1. named import를 사용할 수 있다<br>
+2. import하는 대상을 온전히 로딩하는게 아니고, 그 중 필요한 부분만 destructuring처럼 가져올 수 있어서 메모리에 장점<br>
+3. asynchronous 비동기통신<br>
+
+`createServer`  http 모듈에 정으된 함수<br>
+`function 뒤에 함수명 없음`<br>
+`writeHead(숫자`  숫자 === 상태코드 , ex) 200 정상 404 에러<br><br>
+
+```
+http://naver.com/hyejin?postId=222&cafeId=222
+```
+**? 키=값 & 키2=값2**   ===   `Query String` 이라고 부름 ( 요청방법 1. 주소형태, 2. HTML의 form 방식 )
+<br><br>
+
+### * **GET** <br>
+1. 주고(url)를 통해 전송<br>
+2. 전송 길이 제한<br>
+### 
+### * **POST** <br>
+1. header의 body를 통해 전송<br>
+2. 전송 길이 제한 없음<br>
+3. 약간의 보안 (주소상에서 보이지않는)<br><br>
+
+`EventEmitter` 모든 이벤트 처리가 정의된 기본<span style="color:#e11d21">객체</span> / 이벤트를 사용하려면 이 객체를 <span style="color:#e11d21">재정의</span>해줘야함!!<br>
+`on()` 이벤트를 <span style="color:#e11d21">연결</span>하는 함수<br>
+`emit()` 이벤트를 <span style="color:#e11d21">발생</span>시키는 함수<br><br>
+
+
+* **`app.listen(80);` 서버포트 80 === 브라우저에서 요청시 포트번호 생략하여 사용가능**<br><br>
+
+* **콜백함수를 통한 오류처리**<br>
+1. 동기 불가능 / 비동기 가능<br>
+2. 동시에 불러올경우 **동기부터 출력**한다<br>
--- main.js
+++ main.js
@@ -55,7 +55,13 @@ var app = http.createServer(function(request,response){
             var list = templateList(filelist);
             var template = templateHTML(title, list,
               `<h2>${title}</h2>${description}`,
-              `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
+              `<a href="/create">create</a>
+               <a href="/update?id=${title}">update</a>
+               <form action="delete_process" method="post" onsubmit="return confirm('진짜 삭제할거야?')">
+                <input type="hidden" name="id" value="${title}">
+                <input type="submit" value="delete">
+               </form>               
+               `
             );
             response.writeHead(200);
             response.end(template);
@@ -67,7 +73,7 @@ var app = http.createServer(function(request,response){
         var title = 'WEB - create';
         var list = templateList(filelist);
         var template = templateHTML(title, list, `
-          <form action="http://localhost:3000/create_process" method="post">
+          <form action="/create_process" method="post">
             <p><input type="text" name="title" placeholder="title"></p>
             <p>
               <textarea name="description" placeholder="description"></textarea>
@@ -80,7 +86,7 @@ var app = http.createServer(function(request,response){
         response.writeHead(200);
         response.end(template);
       });
-    } else if(pathname === '/create_process'){
+    } else if(pathname === '/create_process'){ 
       var body = '';
       request.on('data', function(data){
           body = body + data;
@@ -91,6 +97,60 @@ var app = http.createServer(function(request,response){
           var description = post.description;
           fs.writeFile(`data/${title}`, description, 'utf8', function(err){
             response.writeHead(302, {Location: `/?id=${title}`});
+            response.end();
+          })
+      });
+    } else if(pathname === '/update'){ // 수정화면
+      fs.readdir('./data', function(error, filelist){
+        fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
+          var title = queryData.id;
+          var list = templateList(filelist);
+          var template = templateHTML(title, list,
+            `
+            <form action="/update_process" method="post">
+              <input type="hidden" name="id" value="${title}">
+              <p><input type="text" name="title" placeholder="title" value="${title}"></p>
+              <p>
+                <textarea name="description" placeholder="description">${description}</textarea>
+              </p>
+              <p>
+                <input type="submit">
+              </p>
+            </form>
+            `,
+            `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
+          );
+          response.writeHead(200);
+          response.end(template);
+        });
+      });
+    } else if(pathname === '/update_process'){ // 수정 후 submit 까지
+      var body = '';
+      request.on('data', function(data){
+          body = body + data;
+      });
+      request.on('end', function(){
+          var post = qs.parse(body);
+          var id = post.id;
+          var title = post.title;
+          var description = post.description;
+          fs.rename(`data/${id}`, `data/${title}`, function(error){
+            fs.writeFile(`data/${title}`, description, 'utf8', function(err){
+              response.writeHead(302, {Location: `/?id=${title}`});
+              response.end();
+            })
+          });
+      });
+    } else if(pathname === '/delete_process'){ // 삭제 후 submit 까지
+      var body = '';
+      request.on('data', function(data){
+          body = body + data;
+      });
+      request.on('end', function(){
+          var post = qs.parse(body);
+          var id = post.id;
+          fs.unlink(`data/${id}`, function(error){
+            response.writeHead(302, {Location: `/`});
             response.end();
           })
       });
