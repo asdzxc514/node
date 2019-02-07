5359ff78b9e081c4cfe8cfe1185868f025cd046a
김혜진 <asdzxc514@naver.com>
2019-02-08 08:38:17 +0900

    node.js

--- main.js
+++ main.js
@@ -110,12 +110,8 @@ var app = http.createServer(function(request,response){
             <form action="/update_process" method="post">
               <input type="hidden" name="id" value="${title}">
               <p><input type="text" name="title" placeholder="title" value="${title}"></p>
-              <p>
-                <textarea name="description" placeholder="description">${description}</textarea>
-              </p>
-              <p>
-                <input type="submit">
-              </p>
+              <p><textarea name="description" placeholder="description">${description}</textarea></p>
+              <p><input type="submit"></p>
             </form>
             `,
             `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
